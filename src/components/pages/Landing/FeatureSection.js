96ae2cde7ee754fe7b23e00f94c2c9e63debbfe2
Altay Simsek <altaysimsek16@gmail.com>
2023-03-23 01:57:35 +0300

    feat: about section

--- src/components/pages/Landing/FeatureSection.js
+++ src/components/pages/Landing/FeatureSection.js
@@ -1,11 +1,11 @@
 const FeatureSection = () => {
   return (
-    <section className="px-4">
+    <section id="features">
       <h3 className="transition hover:opacity-100 font-bold text-2xl text-white opacity-80">
         Features
       </h3>
       <div className="flex justify-between px-16 mt-5">
-        <div className="flex flex-col items-center p-4 gap-4 h-80 rgBorder rounded-lg">
+        <div className="flex flex-col items-center p-4 gap-4 h-80 rgBorder rounded-lg drop-shadow-md">
           <p className="text-[#81ECAE] font-medium">Custom Shortcuts</p>
           <p className="text-xs font-medium text-white/[.6] text-center">
             Create custom URLs that are easy
@@ -13,7 +13,7 @@ const FeatureSection = () => {
             to remember and reflect your brand.
           </p>
         </div>
-        <div className="flex flex-col items-center p-4 gap-4 h-80 rgBorder rounded-lg">
+        <div className="flex flex-col items-center p-4 gap-4 h-80 rgBorder rounded-lg drop-shadow-md">
           <p className="text-[#81ECAE] font-medium">Analytics</p>
           <p className="text-xs font-medium text-white/[.6] text-center">
             Track clicks and other engagement
@@ -21,7 +21,7 @@ const FeatureSection = () => {
             audience&apos;s behavior.
           </p>
         </div>
-        <div className="flex flex-col items-center p-4 gap-4 h-80 rgBorder rounded-lg">
+        <div className="flex flex-col items-center p-4 gap-4 h-80 rgBorder rounded-lg drop-shadow-md">
           <p className="text-[#81ECAE] font-medium">Superfast</p>
           <p className="text-xs font-medium text-white/[.6] text-center">
             Spend less time managing URLs <br />
--- src/components/pages/Landing/Landing.js
+++ src/components/pages/Landing/Landing.js
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from '@/components/shared/Navbar'
 import SearchSection from './SearchSection'
 import FeatureSection from './FeatureSection'
+import AboutSection from './AboutSection'
 
 const Landing = () => {
   return (
     <>
-        <Navbar/>
-        <SearchSection/>
-        <FeatureSection/>
+      <Navbar />
+      <SearchSection />
+      <FeatureSection />
+      <AboutSection />
     </>
   )
 }
