e99e413b5d620a8503d9ae1917c4166e83bc100a
Altay Simsek <altaysimsek16@gmail.com>
2023-03-28 01:48:39 +0300

    chore: add prisma as orm

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
 export const authOptions = {
 	// Configure one or more authentication providers
