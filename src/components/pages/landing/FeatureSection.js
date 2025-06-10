1f5f394f47505596a7f20edaaad0a15f230f2faf
Görkem Ater <gorkem.ater1@hotmail.com>
2025-06-10 22:19:12 +0300

    feat: update on fatures cards

--- src/components/pages/landing/FeatureSection.js
+++ src/components/pages/landing/FeatureSection.js
@@ -1,5 +1,7 @@
 import { IconBolt, IconChartBar, IconLink } from '@tabler/icons-react';
 
+import SpotlightCard from './SpotlightCards';
+
 const features = [
 	{
 		icon: IconLink,
@@ -27,21 +29,13 @@ const FeatureSection = () => {
 			<h3 className='transition hover:opacity-100 font-bold text-2xl text-white opacity-80'>
 				Features
 			</h3>
-			<div className='flex justify-between px-16 mt-5'>
+			<div className='flex justify-between px-16 mt-5 gap-x-8'>
 				{features.map(({ icon: Icon, title, description }) => (
-					<div
-						key={title}
-						className='group relative flex flex-col items-center p-4 gap-4 h-80 rgBorder rounded-lg drop-shadow-md transition-transform duration-300 hover:-translate-y-2 hover:scale-105'
-					>
-						<Icon
-							size={32}
-							className='text-[#81ECAE] group-hover:rotate-6 transition-transform duration-300'
-						/>
-						<p className='text-[#81ECAE] font-medium'>{title}</p>
-						<p className='text-xs font-medium text-white/[.6] text-center'>
-							{description}
-						</p>
-					</div>
+					<SpotlightCard key={title} className='h-64'>
+						<Icon size={36} className='text-[#81ECAE] mb-4' />
+						<p className='text-2xl font-semibold text-white mb-2'>{title}</p>
+						<p className='text-base text-white/60'>{description}</p>
+					</SpotlightCard>
 				))}
 			</div>
 		</section>
